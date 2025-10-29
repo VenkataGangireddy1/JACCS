3837050ffe6e52e36d999d15c10c5a00953a3d89
Venkata Amarnath Reddy Gangireddy <venkatagangireddy@Venkatas-MacBook-Air.local>
2025-10-29 15:45:47 -0400

    fourth commit

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -3,7 +3,6 @@ import './Title.css'
 function Title() {
   return (
     <div className="title">
-        <h2>Benefits</h2>
         <h2>Our maid service aims to delight every time — that’s our promise</h2>
     </div>
   )
